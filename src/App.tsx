e1b0c01c9e6d3ab7e86f1c493c55f0e433f8bd3e
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2021-12-04 15:35:54 +0300

    lesson 10

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from "./Footer";
+import Technologies from "./Technologies";
+import Header from "./Header";
+
+
+const App = () => {
+  return (
+      <div>
+        <Header/>
+        <Technologies/>
+        <Footer/>
+      </div>);
+}
+
+export default App;
