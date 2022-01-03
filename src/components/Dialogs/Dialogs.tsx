65fc154d5c3e320173594d2604d931e4dfd8c7b0
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2022-01-03 21:08:22 +0300

    lesson 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 //
 // type PropsDialogType={
@@ -8,8 +9,26 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = ()=>{
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    <NavLink to ='/dialogs/1'> Dmitriy</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to ='/dialogs/2'> Svetlana</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to ='/dialogs/3'>Anna</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to ='/dialogs/4'> Sergey</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your it-kamasutra?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
